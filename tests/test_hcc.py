b095b84c58a018b16e8fbb88b87b2cd43ecc6c3b
sarvarimark <50302858+sarvarimark@users.noreply.github.com>
2025-01-04 00:55:20 +0100

    WIP

--- tests/test_hcc.py
+++ tests/test_hcc.py
@@ -2,44 +2,14 @@
 # pylint: disable=C0115
 # pylint: disable=C0116
 from unittest.mock import patch, Mock
-from typing import Any, Optional
 from hcc.hcc import Channel
 
-call_count = 0
-
-def mocked_requests_get(*args: Any, **kwargs: Any) -> Any:
-    class MockResponse:
-        def __init__(self, json_data: Optional[dict[str, str]], status_code: int):
-            self.json_data = json_data
-            self.status_code = status_code
-
-        def json(self):
-            return self.json_data
-
-    global call_count
-    call_count += 1
-
-    if args[0] == 'https://mockserver.com/success':
-        return MockResponse({"key1": "value1"}, 200)
-    if args[0] == 'https://mockserver.com/success_on_third_time':
-        print(call_count)
-        if call_count % 3 == 0:
-            return MockResponse({"key1": "value1"}, 200)
-        return MockResponse(None, 500)
-    if args[0] == 'https://mockserver.com/succes_on_fifth_time':
-        print(call_count)
-        if call_count % 5 == 0:
-            return MockResponse({"key1": "value1"}, 200)
-        return MockResponse(None, 500)
-    if args[0] == 'https://mockserver.com/fail':
-        return MockResponse(None, 500)
-
-def setup_function():
-    global call_count
-    call_count = 0
-
-@patch('hcc.hcc.requests.get', side_effect=mocked_requests_get)
+@patch('hcc.hcc.requests.get')
 def test_channel_get_success(mock_get: Mock):
+    mock_get.side_effect = [
+        Mock(status_code=200, json_data={"key1": "value1"})
+    ]
+
     channel = Channel(
         url="https://mockserver.com/success",
         timeout=1.0,
@@ -50,8 +20,14 @@ def test_channel_get_success(mock_get: Mock):
     assert response.status_code == 200
     assert mock_get.call_count == 1
 
-@patch('hcc.hcc.requests.get', side_effect=mocked_requests_get)
+@patch('hcc.hcc.requests.get')
 def test_channel_get_fail(mock_get: Mock):
+    mock_get.side_effect = [
+        Mock(status_code=500, json_data=None),
+        Mock(status_code=500, json_data=None),
+        Mock(status_code=500, json_data=None)
+    ]
+
     channel = Channel(
         url="https://mockserver.com/fail",
         timeout=1.0,
@@ -62,8 +38,14 @@ def test_channel_get_fail(mock_get: Mock):
     assert response.status_code == 500
     assert mock_get.call_count == 3
 
-@patch('hcc.hcc.requests.get', side_effect=mocked_requests_get)
+@patch('hcc.hcc.requests.get')
 def test_channel_get_success_on_third_time(mock_get: Mock):
+    mock_get.side_effect = [
+        Mock(status_code=500, json_data=None),
+        Mock(status_code=500, json_data=None),
+        Mock(status_code=200, json_data={"key1": "value1"})
+    ]
+
     channel = Channel(
         url="https://mockserver.com/success_on_third_time",
         timeout=1.0,
